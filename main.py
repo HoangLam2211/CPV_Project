1b315a1b4e31ddf5e147edde5405e7a2ce5f0209
Đoàn Đăng Khoa <163561855+ddkhoa2605@users.noreply.github.com>
2024-10-25 21:13:41 +0700

    update
    update more functions (stitch, prepare_lists, get_homography)

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ class Matcher:
         kp, des = self.sift.detectAndCompute(gray, None)  # Phát hiện và tính toán đặc trưng
         return {"kp": kp, "des": des}  # Trả về keypoints và descriptors
 
+
 # Lớp Stitcher để ghép nối các ảnh thành một panorama
 class Stitcher:
     def __init__(
@@ -67,3 +68,49 @@ class Stitcher:
         self.crop_x_max = crop_x_max
         self.crop_y_min = crop_y_min
         self.crop_y_max = crop_y_max
+
+    # Hàm thực hiện ghép nối các ảnh
+    def stitch(self, images=[]):
+        self.images = images  # Danh sách ảnh
+        self.prepare_lists()  # Chuẩn bị các danh sách ảnh
+
+        # Bắt đầu đo thời gian ghép nối
+        start = timeit.default_timer()
+        self.left_shift()  # Ghép từ giữa sang trái
+        self.right_shift()  # Ghép từ giữa sang phải
+        stop = timeit.default_timer()
+        duration = stop - start
+        print("stitching took %.2f seconds." % duration)  # In ra thời gian thực hiện
+
+        # Trả về ảnh kết quả đã cắt nếu có thông số crop
+        if self.crop_x_min and self.crop_x_max and self.crop_y_min and self.crop_y_max:
+            return self.result[
+                self.crop_y_min : self.crop_y_max, self.crop_x_min : self.crop_x_max
+            ]
+        else:
+            return self.result
+
+    # Hàm chuẩn bị các danh sách ảnh để ghép
+    def prepare_lists(self):
+        # Danh sách ảnh trái và phải
+        self.left_list = []
+        self.right_list = []
+
+        self.center_index = int(self.count / 2)  # Xác định ảnh ở giữa
+        self.result = self.images[self.center_index]  # Bắt đầu với ảnh giữa
+
+        # Chia các ảnh thành bên trái và bên phải so với ảnh giữa
+        for i in range(self.count):
+            if i <= self.center_index:
+                self.left_list.append(self.images[i])
+            else:
+                self.right_list.append(self.images[i])
+
+    # Hàm tính toán ma trận Homography giữa hai ảnh
+    def get_homography(self, image_1, image_1_key, image_2, image_2_key, direction):
+        cache_key = "_".join([image_1_key, image_2_key, direction])  # Tạo khóa bộ nhớ đệm
+        homography = self.homography_cache.get(cache_key, None)
+        if homography is None:
+            homography = self.matcher_obj.match(image_1, image_2)  # Tính toán Homography
+            self.homography_cache[cache_key] = homography  # Lưu vào bộ nhớ đệm
+        return homography
